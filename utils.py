b1c8ae478c9321cd09fa16ed8596d0975d01d5ac
IgorT <itarid@gmail.com>
2022-11-17 16:37:31 -0500

    add utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+def job():
+    print('init change')
+    print('change1')
+    print('change1 for branch1')
+    print('change1 for branch3')
+    print('change1 for branch2')
+    print('patch1')
+    print('change2 for branch3')
