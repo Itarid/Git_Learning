94ea9b3a406da5ae9ab5b60eb68f9587d8021f03
IgorT <itarid@gmail.com>
2022-11-17 16:33:50 -0500

    some lines commented

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from utils import *
+# from utils import *
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    job()
+    # job()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
